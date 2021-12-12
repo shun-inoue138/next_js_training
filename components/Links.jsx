55735f8009e5e05587b70e688376fa32f7d262f8
shun-inoue138 <ht8sk13@icloud.com>
2021-12-12 13:42:59 +0900

    LInksコンポーネントのリファクタリング

--- components/Links.jsx
+++ components/Links.jsx
@@ -2,33 +2,40 @@ import React from 'react';
 import classes from './Links.module.css';
 
 const Links = () => {
+  const linkDatas = [
+    {
+      url: 'https://nextjs.org/docs',
+      title: 'Documentation',
+      description: 'Find in-depth information about Next.js features and API.',
+    },
+    {
+      url: 'https://nextjs.org/learn',
+      title: 'Learn',
+      description: 'Learn about Next.js in an interactive course with quizzes!',
+    },
+    {
+      url: 'https://github.com/vercel/next.js/tree/master/examples',
+      title: 'Examples',
+      description: 'Discover and deploy boilerplate example Next.js projects.',
+    },
+    {
+      url:
+        'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+      title: 'Deploy',
+      description:
+        'Instantly deploy your Next.js site to a public URL with Vercel.',
+    },
+  ];
   return (
     <div className={classes.grid}>
-      <a href="https://nextjs.org/docs" className={classes.card}>
-        <h2>Documentation &rarr;</h2>
-        <p>Find in-depth information about Next.js features and API.</p>
-      </a>
-
-      <a href="https://nextjs.org/learn" className={classes.card}>
-        <h2>Learn &rarr;</h2>
-        <p>Learn about Next.js in an interactive course with quizzes!</p>
-      </a>
-
-      <a
-        href="https://github.com/vercel/next.js/tree/master/examples"
-        className={classes.card}
-      >
-        <h2>Examples &rarr;</h2>
-        <p>Discover and deploy boilerplate example Next.js projects.</p>
-      </a>
-
-      <a
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        className={classes.card}
-      >
-        <h2>Deploy &rarr;</h2>
-        <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-      </a>
+      {linkDatas.map((linkData) => {
+        return (
+          <a href={linkData.url} className={classes.card} id={linkData.url}>
+            <h2>{linkData.title} &rarr;</h2>
+            <p>{linkData.description}</p>
+          </a>
+        );
+      })}
     </div>
   );
 };
