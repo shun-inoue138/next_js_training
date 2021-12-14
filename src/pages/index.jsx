ef26a7b290b2b867ac91fd86bbfe7d0c98c50cd4
shun-inoue138 <ht8sk13@icloud.com>
2021-12-14 09:53:05 +0900

    Array型のstateを作成。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,8 +10,15 @@ import { useCallback, useState } from 'react';
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState('hoge');
+  const [isShow, setIsShow] = useState(true);
+  const [numAry, setNumAry] = useState([1, 2, 5]);
   const handleClick = () => {
     setCount((count) => ++count);
+  };
+  const handleNumAry = () => {
+    const additionalNum = Math.floor(Math.random() * 100);
+    setNumAry((numAry) => [...numAry, additionalNum]);
   };
   return (
     <div className={styles.container}>
@@ -21,13 +28,40 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <div>{count}</div>
+      {isShow && <div>{count}</div>}
+      <button
+        onClick={() => {
+          setIsShow((isShow) => !isShow);
+        }}
+      >
+        表示切り替え
+      </button>
       <button
         onClick={() => {
           handleClick();
         }}
       >
-        ボタン
+        カウントアップ
+      </button>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => {
+          if (e.target.value.length > 5) {
+            alert('５文字以下で入力してください');
+            return;
+          }
+          setText(e.target.value.trim());
+        }}
+      />
+      <h3>ここに数値を出力</h3>
+      <div>{numAry.map((num) => num)}</div>
+      <button
+        onClick={() => {
+          handleNumAry();
+        }}
+      >
+        ランダムで数値を追加
       </button>
       <Main>Index</Main>
       <Footer />
