eb6513351e8b19665eef47ab8666f11e33eb8d4a
shun-inoue138 <ht8sk13@icloud.com>
2021-12-14 17:59:27 +0900

    Linksコンポーネント内にてstateを定義するバージョン。

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -1,38 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Links.module.css';
 
+const initialLinkItems = [
+  {
+    url: 'https://nextjs.org/docs',
+    title: 'Documentation',
+    description: 'Find in-depth information about Next.js features and API.',
+  },
+  {
+    url: 'https://nextjs.org/learn',
+    title: 'Learn',
+    description: 'Learn about Next.js in an interactive course with quizzes!',
+  },
+  {
+    url: 'https://github.com/vercel/next.js/tree/master/examples',
+    title: 'Examples',
+    description: 'Discover and deploy boilerplate example Next.js projects.',
+  },
+  {
+    url:
+      'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+    title: 'Deploy',
+    description:
+      'Instantly deploy your Next.js site to a public URL with Vercel.',
+  },
+];
+
 const Links = () => {
-  const linkDatas = [
-    {
-      url: 'https://nextjs.org/docs',
-      title: 'Documentation',
-      description: 'Find in-depth information about Next.js features and API.',
-    },
-    {
-      url: 'https://nextjs.org/learn',
-      title: 'Learn',
-      description: 'Learn about Next.js in an interactive course with quizzes!',
-    },
-    {
-      url: 'https://github.com/vercel/next.js/tree/master/examples',
-      title: 'Examples',
-      description: 'Discover and deploy boilerplate example Next.js projects.',
-    },
-    {
-      url:
-        'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
-      title: 'Deploy',
-      description:
-        'Instantly deploy your Next.js site to a public URL with Vercel.',
-    },
-  ];
+  const [linkItems, setLinkItems] = useState(initialLinkItems);
+  const handleReduce = () => {
+    setLinkItems((prevLinkItems) =>
+      prevLinkItems.slice(0, prevLinkItems.length - 1)
+    );
+  };
+
   return (
     <div className={classes.grid}>
-      {linkDatas.map((linkData) => {
+      <div>
+        <button
+          onClick={() => {
+            handleReduce();
+          }}
+        >
+          減らす
+        </button>
+      </div>
+      {linkItems.map((item) => {
         return (
-          <a href={linkData.url} className={classes.card} key={linkData.url}>
-            <h2>{linkData.title} &rarr;</h2>
-            <p>{linkData.description}</p>
+          <a href={item.url} className={classes.card} key={item.url}>
+            <h2>{item.title} &rarr;</h2>
+            <p>{item.description}</p>
           </a>
         );
       })}
